331695e9082ff6f4d93e8768b0bf72211bab3b33
Sam Light <samlight1994@gmail.com>
2017-01-18 10:02:14 +0000

    Written avtransport play function

--- avtransport.py
+++ avtransport.py
@@ -4,6 +4,7 @@ class AVTransport:
 
     SERVICE = "urn:schemas-upnp-org:service:AVTransport:1"
 
+    instance_id = '0'
     def __init__(self, dev):
         self.device = dev
 
@@ -13,6 +14,14 @@ class AVTransport:
     def set_transport_url(self, url):
         req = self.create_req()
         req.set_action('SetAVTransportURI')
-        req.add_attribute('InstanceID', '0')
+        req.add_attribute('InstanceID', self.instance_id)
         req.add_attribute('CurrentURI', url)
         return control.send_request(req)
+
+    def play(self):
+        req = self.create_req()
+        req.set_action('Play')
+        req.add_attribute('InstanceID', self.instance_id)
+        req.add_attribute('Speed', '1')
+        return control.send_request(req)
+
