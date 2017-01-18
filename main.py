ca7b1349bd9cd04f952690fba8040bc32cc0b6ab
Sam Light <samlight1994@gmail.com>
2017-01-18 09:52:52 +0000

    testing all the new stuff out.

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import device
 from ssdp import SSDP
+from connectionmanager import ConnectionManager
+from avtransport import AVTransport
+from server import Server
 
-desired_service = "urn:schemas-upnp-org:service:AVTransport:1"
+server = Server()
+server.start()
 
 s = SSDP()
 s.scan()
-devices = device.filter_devices_by_service_type(
-    device.get_devices(s.responses), desired_service)
+devices = device.get_devices(s.responses)
+devices = device.filter_devices_by_service_type(devices, AVTransport.SERVICE)
+devices = device.filter_devices_by_service_type(devices, ConnectionManager.SERVICE)
 
+dev = None
 for d in devices:
-    print(d.friendly_name)
+    if(d.friendly_name == "HiFiPI"): dev = d
 
+tran = AVTransport(dev)
+conn = ConnectionManager(dev)
+print(conn.get_protocol_info().body()['s:Envelope']['s:Body'])
+#print(tran.set_transport_url("http://192.168.1.104/"))
+server.wait()
