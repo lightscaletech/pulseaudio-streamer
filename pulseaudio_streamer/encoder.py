3730155143da098448675216e6d9d349f287f925
Sam Light <samlight1994@gmail.com>
2017-01-25 16:46:32 +0000

    Run encoder commands seperately without shell

--- pulseaudio_streamer/encoder.py
+++ pulseaudio_streamer/encoder.py
@@ -4,7 +4,8 @@ import errno
 import logging
 import time
 from select import select
-
+import time
+import threading
 
 class Encoder:
     def __init__(self, m):
@@ -16,22 +17,31 @@ class Encoder:
         out_format = 'mp3'
         out_rate = '320k'
 
+        self.stop_encoding = threading.Event()
+
         self.enc_out = '/tmp/%s.out' % m['name']
-        self.enc_fd = None
+        self.stream_path = '/tmp/%s.stream' % m['name']
+
+        self.stream_fd_in = None
+        self.stream_fd_out = None
+
+        self.enc_fd_out = None
+        self.enc_fd_in = None
+
         self.enc_proc = None
+        self.rec_proc = None
 
-        prec_com = ('parec --format=%s --rate=%i -d %s | ' %
-                    (in_format, in_rate, in_dev))
+        self.prec_com = ['/usr/bin/parec',
+                         ('--format=%s' % in_format),
+                         ('--rate=%i' % in_rate),
+                         '-d', ('%s' % in_dev)]
 
-        ffmpeg_com = (('ffmpeg -ac %i ' % in_chan) +
-                      ('-ar %i ' % in_rate) +
-                      ('-f %s ' % in_format) +
-                      '-i - ' +
-                      ('-b:a %s ' % out_rate) +
-                      ('-f %s ' % out_format) +
-                      ('- > %s' % self.enc_out))
-
-        self.command = prec_com + ffmpeg_com
+        self.ffmpeg_com = ['/usr/bin/ffmpeg', '-ac', ('%i' % in_chan),
+                           '-ar', ('%i' % in_rate),
+                           '-f', in_format,
+                           '-i', '-',
+                           '-b:a', out_rate,
+                           '-f', out_format, '-']
 
         self.devnull = open(os.devnull, 'wb')
 
@@ -43,31 +53,75 @@ class Encoder:
         except OSError as e:
             if e.errno == errno.EEXIST:
                 logging.debug("Pulseaudio Fifo file '%s' already exists" % f)
+            else: raise
+        except: raise
 
     def start(self):
         logging.debug("Starting encoding")
+
+        self.stop_encoding.clear()
+
         self.create_fifo(self.enc_out)
-        self.enc_proc = subprocess.Popen(self.command, shell=True,
-                                        stderr=self.devnull, stdout=self.devnull)
-        self.enc_fd = open(self.enc_out, 'rb')
+        self.create_fifo(self.stream_path)
+
+        self.enc_fd_out = os.open(self.enc_out, os.O_RDONLY | os.O_NONBLOCK)
+        self.enc_fd_in = os.open(self.enc_out, os.O_WRONLY | os.O_NONBLOCK)
+        self.stream_fd_out = os.open(self.stream_path, os.O_RDONLY | os.O_NONBLOCK)
+        self.enc_proc = subprocess.Popen(self.ffmpeg_com,
+                                         stdin=self.stream_fd_out,
+                                         stderr=self.devnull,
+                                         stdout=self.enc_fd_in)
+
+        self.stream_fd_in = os.open(self.stream_path, os.O_WRONLY | os.O_NONBLOCK)
+        self.rec_proc = subprocess.Popen(self.prec_com,
+                                         stderr=self.devnull,
+                                         stdout=self.stream_fd_in)
+
+    @staticmethod
+    def stop_proc(proc):
+        if proc: proc.terminate()
+
+    @staticmethod
+    def close_fd(fd):
+        if fd: os.close(fd)
 
     def stop(self):
         logging.debug("Stopping encoding")
-        if self.enc_proc:
-            self.enc_proc.terminate()
-            self.enc_proc = False
-        if self.enc_fd:
-            self.enc_fd.close()
-            self.enc_fd = None
+
+        self.stop_encoding.set()
+
+        self.enc_proc = self.stop_proc(self.enc_proc)
+        self.rec_proc = self.stop_proc(self.rec_proc)
+        logging.debug('\t- Shutdown encoder proccesses')
+
+        self.enc_fd_out = self.close_fd(self.enc_fd_out)
+        logging.debug('\t- Closed encoder out')
+        self.enc_fd_in = self.close_fd(self.enc_fd_in)
+        logging.debug('\t- Closed encoder in')
+        self.stream_fd_out = self.close_fd(self.stream_fd_out)
+        logging.debug('\t- Stream encoder out')
+        self.stream_fd_in = self.close_fd(self.stream_fd_in)
+        logging.debug('\t- Stream encoder in')
+
         try:
             os.remove(self.enc_out)
+            os.remove(self.stream_path)
         except os.error as err:
             if err.errno == errno.ENOENT: pass
             else: raise
 
-
-
+        logging.debug('\t- Removed pipes')
+        logging.debug('Encoding stopped')
 
     def read(self):
-        return self.enc_fd.read(1024 * 8)
+        while True:
+            try:
+                return os.read(self.enc_fd_out, 1024 * 8)
+            except os.error as err:
+                if errno.EAGAIN == err.errno:
+                    if self.stop_encoding.wait(0): return ''
+                    continue
+                else:
+                    logging.debug('Failed to read from encoder output: %s' % errno.errorcode[err.errno])
+                    raise
 
